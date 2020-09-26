6e5d23401a38c1caf7c7fc4ae618e056e0f470d9
Camila Rojas Prado <camilarojaspr123@gmail.com>
2020-09-26 00:33:47 -0500

    temperatura_edad

---
+++ tablas.py
@@ -0,0 +1,22 @@
+#script: tablas
+
+'''programa que solicita al usuario el numero de una tabla de
+multiplicar, y vizualizar dicha tabla con la serie del 1 al 10.
+Nota :No debe usar expresion que no se haya trabajado en clase.
+'''
+#inputs
+print("programa para determinar la tabla de un numero")
+print("introduce un numero")
+n=int(input())
+
+#proocess and outputs
+print(n," x 1 =",n*1)
+print(n," x 2 =",n*2)
+print(n," x 3 =",n*3)
+print(n," x 4 =",n*4)
+print(n," x 5 =",n*5)
+print(n," x 6 =",n*6)
+print(n," x 7 =",n*7)
+print(n," x 8 =",n*8)
+print(n," x 9 =",n*9)
+print(n," x 10 =",n*10)
---
+++ temperatura.py
@@ -0,0 +1,5 @@
+#string: temperatura 
+Gc = float(input("Digite grados Centigrados: "))
+Gk = (Gc + 273.15)
+print("el valor de los grados kelvin es el siguiente: ",Gk)
+                 
