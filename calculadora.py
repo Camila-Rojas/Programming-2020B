b238b08163fce57b2dc6de08698ccc2441b3e1e7
Camila Rojas Prado <camilarojaspr123@gmail.com>
2020-09-25 23:11:11 -0500

    operaciones basicas

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+#script: calculadora
+'''operaciones basicas'''
+n1=int
+n2=int
+n1=5
+n2=6
+
+suma=0
+resta=0
+multiplicacion=0
+division=0
+
+suma = n1+n2
+resta = n1-n2
+multiplicacion = n1*n2
+division = n1/n2
+
+print("la suma es: ",suma)
+print("la resta es: ",resta)
+print("la multiplicacion es: ",multiplicacion)
+print("la division es: ",division)
+    
+
+   
---
+++ edad.py
@@ -0,0 +1,19 @@
+#script: edad
+
+'''calcula la edad'''
+n1=2020
+nombre=0
+nacimiento=0
+edad=0
+print("escriba su nombre: ")
+
+nombre = input()
+
+print("escriba su año de nacimiento: ")
+nacimiento = int (input())
+
+#process
+edad = n1-nacimiento
+
+#output
+print("la edad de','nombre','es'", edad)
